c2a4e0e7236e341bf2133fc6238b5b970c935769
Manuel Cheveste <manuelcheveste@gmail.com>
2020-01-22 17:21:43 -0300

    Added hot reloading and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
+  state = {
+    count: 0
+  }
+
   render() {
-    return <h1>Hello World!!</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
+        <button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
+      </div>
+    )
   }
 }
 
-export default App
+export default hot(module)(App)
